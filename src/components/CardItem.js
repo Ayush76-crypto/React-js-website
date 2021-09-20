5017451106183a4b151f1914859135c6067bebe8
Ayush Verma <78358911+Ayush76-crypto@users.noreply.github.com>
2021-09-20 14:02:33 +0530

    Update CardItem.js

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function CardItem(props) {
+function CardItem( props ) {
+  
   return (
     <>
       <li className="cards__item">
